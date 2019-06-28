069a1afbb46d57b129f63583752fbb67f68cb5c6
JoshKain <JoshKain0612@gmail.com>
2019-06-28 08:00:46 +0100

    Css for header complete

--- src/components/ArticlePage/ArticlePage.jsx
+++ src/components/ArticlePage/ArticlePage.jsx
@@ -9,7 +9,7 @@ import PostCommentComponent from "../CommentList/PostCommentComponent";
 moment().format();
 
 export default class ArticlePage extends Component {
-  state = { article: [] };
+  state = { article: [], isLoading: false };
 
   render() {
     const {
@@ -20,40 +20,49 @@ export default class ArticlePage extends Component {
       body,
       article_id
     } = this.state.article;
-
-    return (
-      this.state.article && (
-        <div className="single-article-container">
-          <ul id="container">
-            <div>
-              <h3 id="article-title">{title}</h3>
-              <p>
-                Posted by: {author} {moment(created_at).fromNow()}{" "}
-              </p>
-            </div>
-            <p>{body}</p>
-            <div className="Voter">
-              <VoterComponent votes={votes} article_id={article_id} />
-            </div>
-          </ul>
-          <div className="comment-container">
-            <PostCommentComponent
-              article_id={article_id}
-              username={this.props.username}
-            />
-            <CommentsList
-              id={this.props.article_id}
-              username={this.props.username}
-            />
-          </div>
+    const { isLoading } = this.state;
+    if (isLoading === false) {
+      return (
+        <div className="loader">
+          <div className="outer" />
+          <div className="middle" />
+          <div className="inner" />
         </div>
-      )
+      );
+    }
+    return (
+      <div className="single-article-container">
+        <ul id="container">
+          <div>
+            <h3 id="article-title">{title}</h3>
+            <p>
+              Posted by: {author} {moment(created_at).fromNow()}{" "}
+            </p>
+          </div>
+          <p>{body}</p>
+          <div className="Voter">
+            <VoterComponent votes={votes} article_id={article_id} />
+          </div>
+        </ul>
+        <div className="comment-container">
+          <PostCommentComponent
+            article_id={article_id}
+            username={this.props.username}
+          />
+          <CommentsList
+            id={this.props.article_id}
+            username={this.props.username}
+          />
+        </div>
+      </div>
     );
   }
+
   componentDidMount() {
     const { article_id } = this.props;
     api.getArticleById({ article_id }).then(article => {
       this.setState({ article });
+      this.setState({ isLoading: true });
     });
   }
 }
--- src/components/HeaderComponents/Header.jsx
+++ src/components/HeaderComponents/Header.jsx
@@ -5,7 +5,10 @@ export default function Header() {
   return (
     <div>
       <header>
-        <h1 className="header">NC's Daily-Dev's</h1>
+        <h1 className="header">
+          <span className="NC">NC</span>'s Daily-
+          <span className="NC">Dev's</span>
+        </h1>
       </header>
     </div>
   );
