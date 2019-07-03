a1515dc546e7cabed3477d4d4571e763fc361e22
JoshKain <JoshKain0612@gmail.com>
2019-07-03 17:44:32 +0100

    css complete on userPage

--- src/components/UserComponents/UserPage.jsx
+++ src/components/UserComponents/UserPage.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import * as api from "../api";
 import ArticleCard from "../ArticlesComponents/ArticleCard";
-import { Button } from "@material-ui/core";
 import Error from "../ErrorComponent/Error";
+import "./UserPage.css";
 
 export default class UserPage extends Component {
   state = {
@@ -46,50 +46,64 @@ export default class UserPage extends Component {
       );
     }
     return (
-      <div className="each-user">
-        <h1>{name}</h1>
-        <img
-          src={avatar_url}
-          alt={username}
-          style={{ width: 200, height: 200, borderRadius: 100 }}
-        />
-
-        <form>
-          <label>
-            Add An Article:
-            <input
-              type="text"
-              name="Title...."
-              value={this.state.title}
-              onChange={this.handleTitle}
-              placeholder="Title"
-            />{" "}
-            <input
-              type="text"
-              name="body"
-              value={this.state.body}
-              onChange={this.handleBody}
-              placeholder="Body...."
-            />{" "}
-            <select onChange={this.handleTopic}>
-              {topics.map(topic => {
-                return <option key={topic.slug}>{topic.slug}</option>;
-              })}
-            </select>
-          </label>
-          <Button
-            onClick={this.handleSubmit}
-            variant="contained"
-            color="primary"
-          >
-            Submit New Article
-          </Button>
-          {moreLetters && (
-            <p className="CommentTag">
-              Title or Body Space Needs Be Filled in Please
-            </p>
-          )}
-        </form>
+      <div className="topic-container">
+        <div className="user">
+          <h1>{name}</h1>
+          <img
+            src={avatar_url}
+            alt={username}
+            style={{ width: 200, height: 200, borderRadius: 100 }}
+          />
+        </div>
+        <h3>Add An Article:</h3>
+        <div className="topics-form">
+          <form>
+            <div className="sort-Content">
+              <div className="labels">
+                Topic:
+                <select onChange={this.handleTopic}>
+                  {topics.map(topic => {
+                    return (
+                      <option className="sort-options" key={topic.slug}>
+                        {topic.slug}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <label className="labels">
+                Title:
+                <input
+                  type="text"
+                  name="Title...."
+                  value={this.state.title}
+                  onChange={this.handleTitle}
+                  placeholder="Title"
+                  className="input"
+                />{" "}
+              </label>
+              <label>
+                Body:
+                <input
+                  type="text"
+                  name="body"
+                  value={this.state.body}
+                  onChange={this.handleBody}
+                  placeholder="Body...."
+                  className="input"
+                />{" "}
+              </label>
+              <button className="submit-buttons" onClick={this.handleSubmit}>
+                Submit New Article
+              </button>
+            </div>
+            {moreLetters && (
+              <p className="CommentTag">
+                Title or Body Space Needs Be Filled in Please
+              </p>
+            )}
+          </form>
+        </div>
         {articles.length >= 1 &&
           articles.map(article => {
             return <ArticleCard key={article.article_id} article={article} />;
